53a9b08ab81f3416e3f9954f40281bbc850e9465
Poseydon42 <vvmposeydon@gmail.com>
2025-05-21 20:25:05 +0100

    IR: Added Eq and implemented Display for Ty

--- src/ir/ty.rs
+++ src/ir/ty.rs
@@ -1,4 +1,14 @@
-#[derive(Clone, Debug, PartialEq)]
+use std::fmt::*;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Ty {
     Int, // FIXME: i32 for now, add more later
+}
+
+impl Display for Ty {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match self {
+            Ty::Int => write!(f, "int"),
+        }
+    }
 }

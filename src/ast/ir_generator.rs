fcfbe34752eb0c5beda0519f8eaa691240e2aa22
Poseydon42 <vvmposeydon@gmail.com>
2025-06-01 22:20:51 +0100

    AST: added IR lowering for variable declarations and references

--- src/ast/ir_generator.rs
+++ src/ast/ir_generator.rs
@@ -1,9 +1,11 @@
 use crate::ast::*;
 use crate::ir;
+use std::collections::HashMap;
 
 pub struct IRGenerator {
     module: ir::Module,
     function: Option<ir::Function>,
+    variables: HashMap<String, ir::Value>,
 }
 
 impl IRGenerator {
@@ -11,6 +13,7 @@ impl IRGenerator {
         Self {
             module: ir::Module::new(),
             function: None,
+            variables: HashMap::new(),
         }
     }
 
@@ -31,6 +34,7 @@ impl Visitor<'_, Option<ir::Value>> for IRGenerator {
         let bb = ir::BasicBlock::new(func_decl.name.text().to_owned());
         let function = ir::Function::new(func_decl.name.text().to_owned(), bb);
         self.function = Some(function);
+        self.variables.clear();
 
         func_decl.body.iter().for_each(|stmt| { 
             self.visit_stmt(stmt);
@@ -43,12 +47,37 @@ impl Visitor<'_, Option<ir::Value>> for IRGenerator {
         None
     }
 
+    fn visit_variable_decl(&mut self, variable: &'_ VariableDecl, _node: &'_ Decl) -> Option<ir::Value> {
+        assert!(self.function.is_some(), "Variable declaration outside of function is not supported");
+
+        let ty = variable.ty.resolved.as_ref().expect("Variable declaration must have a resolved AST type during IR generation");
+        let allocate = ir::Instruction::Allocate(lower_type(ty));
+        let ptr =self.get_bb_mut().append_named_instruction(allocate, variable.name.text().to_owned()).1.unwrap();
+
+        let init = self.visit_expr(&variable.init).expect("Variable initialization expression must produce a node");
+        let store = ir::Instruction::Store { value: init, location: ptr.clone() };
+        self.get_bb_mut().append_instruction(store);
+        self.variables.insert(variable.name.text().to_owned(), ptr.clone());
+        
+        Some(ptr)
+    }
+
     fn visit_integer_literal_expr(&mut self, expr: &'_ ast::IntegerLiteralExpr, _node: &'_ ast::Expr) -> Option<ir::Value> {
         // FIXME: this is so, so bad. We should use some sort of binary representation of an
         //        "abstract typed value" instead of relying on Rust's integer parsing functionality
         let value: i32 = expr.raw_value.text().parse().expect("Value of an integer literal must be a valid 32 bit signed integer");
         let value = ir::Value::constant(ir::Constant::int(value));
         Some(value)
+    }
+
+    fn visit_variable_reference_expr(&mut self, expr: &'_ VariableReferenceExpr, _node: &'_ Expr) -> Option<ir::Value> {
+        let ptr = self.variables.get(expr.name.text())
+            .expect("Variable reference must refer to a previously declared variable");
+        let load = ir::Instruction::Load { 
+            location: ptr.clone(), 
+            ty: lower_type(expr.ty.as_ref().expect("Variable reference nodes must have a resolved type during IR generation"))
+        };
+        self.get_bb_mut().append_instruction(load).1
     }
 
     fn visit_binary_expr(&mut self, expr: &BinaryExpr, _node: &Expr) -> Option<ir::Value> {
@@ -68,5 +97,17 @@ impl Visitor<'_, Option<ir::Value>> for IRGenerator {
         self.get_bb_mut().append_instruction(ret);
 
         None
+    }
+}
+
+fn lower_type(ty: &ResolvedType) -> ir::Ty {
+    match ty {
+        ResolvedType::BuiltIn(builtin_ty) => lower_builtin_type(builtin_ty),
+    }
+}
+
+fn lower_builtin_type(builtin_ty: &BuiltInType) -> ir::Ty {
+    match builtin_ty {
+        BuiltInType::Int => ir::Ty::Int,
     }
 }
